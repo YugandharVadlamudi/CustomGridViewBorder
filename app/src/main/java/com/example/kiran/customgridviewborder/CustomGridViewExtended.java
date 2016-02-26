cb1ae939a96c43615731ea851bbd3565b4141eed
yugandhar <yugandhardcs21@gmail.com>
2016-02-26 13:01:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kiran/customgridviewborder/CustomGridViewExtended.java
@@ -0,0 +1,38 @@
+package com.example.kiran.customgridviewborder;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Kiran on 19-11-2015.
+ */
+public class CustomGridViewExtended extends BaseAdapter {
+    String[]ImgName;
+    int[]IMmvviews;
+    Context context;
+    CustomGridViewExtended(Context context,String[] ImgName,int[] Imvviews)
+    {
+
+    }
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/kiran/customgridviewborder/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.kiran.customgridviewborder;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class MainActivity extends Activity {
+String[] ImViews={R.drawable.}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
